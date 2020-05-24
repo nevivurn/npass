d1acfc7aa4963ea9bd28f567bd947ed8683bb30c
Yongun Seong <nevivurn@riseup.net>
2020-05-25 01:09:05 +0900

    Add init command

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/nevivurn/npass/store"
 	"github.com/urfave/cli/v2"
 )
 
@@ -15,9 +18,52 @@ func main() {
 }
 
 func newApp() *cli.App {
-	return &cli.App{
+	cmd := &cli.App{
+		Flags: []cli.Flag{
+			&cli.PathFlag{
+				Name:        "db",
+				EnvVars:     []string{"NPASS_DB"},
+				DefaultText: "~/.npass.db",
+			},
+		},
+
 		Commands: []*cli.Command{
-			cmdKey(),
+			cmdInit(),
 		},
 	}
+
+	cmd.Before = func(ctx *cli.Context) error {
+		if !ctx.IsSet("db") {
+			ctx.Set("db", filepath.Join(os.Getenv("HOME"), ".npass.db"))
+		}
+		return nil
+	}
+
+	return cmd
+}
+
+func cmdInit() *cli.Command {
+	cmd := &cli.Command{
+		Name: "init",
+	}
+
+	cmd.Action = func(ctx *cli.Context) error {
+		name := ctx.Path("db")
+
+		_, err := os.Stat(name)
+		if !errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("database already exists")
+		}
+
+		st, err := store.NewInit(name)
+		if err != nil {
+			return err
+		}
+		defer st.Close()
+
+		fmt.Fprintln(ctx.App.Writer, "initialized a new db at", name)
+		return nil
+	}
+
+	return cmd
 }
