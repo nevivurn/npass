e14759c1b08eb28f804603eaec0c7957aafd7d00
Yongun Seong <nevivurn@riseup.net>
2020-05-25 00:48:16 +0900

    Remove debugging code

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 
-	"github.com/nevivurn/npass/pinentry"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	pass, err := pinentry.ReadPassword(context.Background())
-	fmt.Printf("%q %v\n", pass, err)
-
 	if err := newApp().Run(os.Args); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
